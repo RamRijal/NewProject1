b74cd7bfdf18e0947e5f793c74fe9da55f352fdd
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-06-07 01:38:37 +0545

    Category UI Update

--- quiz_app/app/category/page.tsx
+++ quiz_app/app/category/page.tsx
@@ -1,8 +1,123 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
 
 const Category = () => {
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
+
+    if (loading) {
+        return <h1 className='flex justify-center items-center mt-72'>Loading....</h1>;
+    }
+
+    const categories = [{
+        name: 'Html',
+        link: '/html',
+        color: '#005A6D'
+    },
+    {
+        name: 'Css',
+        link: '/css',
+        color: '#B700FB'
+    },
+    {
+        name: 'Javascript',
+        link: '/javascript',
+        color: '#491C91'
+    },
+    {
+        name: 'Tailwind',
+        link: '/tailwind',
+        color: '#F8AF00'
+    },
+    {
+        name: 'React',
+        link: '/react',
+        color: '#61617A'
+    },
+
+    {
+        name: 'Next',
+        link: '/next',
+        color: '#165C92'
+    },
+
+    {
+        name: 'Typescript',
+        link: '/typescript',
+        color: '#6795FA'
+    }
+        ,
+    {
+        name: 'Git',
+        link: '/git',
+        color: '#F80300'
+    }
+        ,
+    {
+        name: 'Github',
+        link: '/github',
+        color: '#02BDC6'
+    }
+        ,
+    {
+        name: 'Node',
+        link: '/node',
+        color: '#7C00FB'
+    }
+        ,
+    {
+        name: 'Express',
+        link: '/express',
+        color: '#165C92'
+    }
+        ,
+    {
+        name: 'MongoDB',
+        link: '/mongodb',
+        color: '#59C7A0'
+    }
+        ,
+    {
+        name: 'Mongoose',
+        link: '/mongoose',
+        color: '#BD5683'
+    }
+        ,
+    {
+        name: 'Postman',
+        link: '/postman',
+        color: '#C3EB50'
+    }
+
+    ]
     return (
-        <div>Category</div>
+        <>
+            <section className="py-4 bg-gradient-to-b from-blue-100 to-blue-400 sm:py-8 lg:py-8">
+                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-full">
+                    <h1 className='text-center  my-16 text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl'>Choose your Category</h1>
+                    <div className="grid grid-cols-1 md:grid-cols-2  gap-12 px-4 md:px-16 py-12 ">
+                        {categories.map((item, index) => {
+                            return (
+                                <Link key='index' href={item.link}>
+                                    <button style={{ backgroundColor: item.color }} color={item.color}
+                                        className="h-20 md:h-24 w-full text-2xl font-semibold text-gray-900 rounded-lg shadow-lg">
+                                        {item.name}
+                                    </button>
+                                </Link>
+                            )
+                        })
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
--- quiz_app/components/landing/hero.tsx
+++ quiz_app/components/landing/hero.tsx
@@ -28,20 +28,38 @@ const Hero = () => {
     }
     return (
         <>
-            <div className="min-vh-100 flex flex-col justify-center items-center gap-8 mt-28 pt-12">
-                <Image className='absolute -z-10 -left-20 top-10' alt='image1' height={700} width={700} src='/136881410_471fe6bf-1dc4-4281-9aa5-238e6445247d.svg' />
+            <div className="h-screen bg-gradient-to-b from-slate-200 to-blue-300 flex flex-col justify-center items-center gap-8 ">
+                <Image className='absolute z-10 -left-32 top-10' alt='image1' height={700} width={700} src='/136881410_471fe6bf-1dc4-4281-9aa5-238e6445247d.svg' />
 
-                <Image className='absolute right-4 top-20' alt='image1' height={500} width={500} src='/136881254_39eb0bae-46c4-44c7-bff3-0c3311dc0bbd.svg' />
+                <Image className='absolute right-2 top-24' alt='image1' height={500} width={500} src='/136881254_39eb0bae-46c4-44c7-bff3-0c3311dc0bbd.svg' />
+                <div className="z-40 flex flex-col justify-center items-center gap-7 my-16">
+                    <h1 className='text-lg font-bold text-gray-700'>Choose your topic, play and learn</h1>
+                    <h1 className='text-6xl font-bold text-gray-700'>&quot;Test Your Tech Knowledge&quot;</h1>
+                    <p className='text-xl font-bold text-gray-700'>For your own self-improvement</p>
+                    <div className="z-50">
+                        <Link href='/login'>
+                            <button className='bg-amber-300 hover:bg-yellow-500  text-gray-800 font-bold py-3 px-6 rounded-full transition-all duration-200 hover:scale-105 hover:shadow-lg'>Get Started</button>
+                        </Link>
+                    </div>
+                    <p className='text-lg font-semibold text-gray-700 mt-3'>Wanna know more about us?</p>
 
-                <h1 className='text-xl font-bold text-gray-700'>Choose your topic, play and learn</h1>
-                <h1 className='text-6xl font-bold text-gray-700'>&quot;Test Your Tech Knowledge&quot;</h1>
-                <p className='text-xl font-bold text-gray-700'>For your own self-improvement</p>
-                <div className="">
-                    <Link href='/login'>
-                        <button className='bg-amber-300 hover:bg-yellow-500  text-gray-800 font-bold py-3 px-6 rounded-full transition-all duration-200 hover:scale-105 hover:shadow-lg'>Get Started</button>
-                    </Link>
-                </div>
-            </div>
+                    <div className="flex justify-center items-center gap-4">
+                        <Link href='/'>
+                            <Image alt='image1' height={35} width={35} src='/facebook-svgrepo-com.svg' />
+                        </Link>
+                        <Link href='/'>
+                            <Image alt='image2' height={40} width={40} src='/instagram-svgrepo-com.svg' />
+                        </Link>
+                        <Link href='/'>
+                            <Image alt='image3' height={32} width={32} src='/linkedin-round-svgrepo-com.svg' />
+                        </Link>
+                        <Link href='/'>
+                            <Image alt='image4' height={30} width={30  } src='/github-142-svgrepo-com.svg' />
+                        </Link>
+                    </div >
+
+                </div >
+            </div >
         </>)
 }
 
